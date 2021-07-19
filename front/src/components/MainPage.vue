0c3dc01b7093319c4e8b0162ded9ff35dc6f67da
봉대현 <pkjeogus@gmail.com>
2021-07-19 14:21:37 +0900

    Frontend-메인페이지/회원가입

--- front/src/components/MainPage.vue
+++ front/src/components/MainPage.vue
@@ -22,12 +22,14 @@
                                 <v-text-field
                                     label="Email"
                                     prepend-inner-icon="mdi-email"
+                                     v-model="login_email"
                                 ></v-text-field>
                             </v-row>
                             <v-row>
                                 <v-text-field
                                     label="Password"
                                     prepend-inner-icon="mdi-lock"
+                                    v-model="login_pwd"
                                 ></v-text-field>
                             </v-row>
                             <v-row class="text-right">
@@ -37,7 +39,7 @@
                                     dark
                                     
                                 >
-                                    Accept
+                                    로그인
                                     <v-icon
                                     dark
                                     right
@@ -49,26 +51,69 @@
                         </v-container>
                     </v-tab-item>
                     <v-tab-item>
-                        
+                        <v-container>
+                        <v-row>
+                            <v-text-field
+                                label="Email"
+                                prepend-inner-icon="mdi-email"
+                                
+                            ></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-text-field
+                                label="Password"
+                                prepend-inner-icon="mdi-lock"
+                               
+                            ></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-text-field
+                                label="Password Confirm"
+                                prepend-inner-icon="mdi-lock"
+                            ></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-text-field
+                                label="Nick Name"
+                                prepend-inner-icon="mdi-text"
+                            ></v-text-field>
+                        </v-row>
+                        <v-row>
+                            <v-select
+                            :items="items"
+                            label="Language"
+                            ></v-select>
+                        </v-row>
+                        <v-row class="text-right">
+                            <v-btn
+                                class="ma-2"
+                                color="primary"
+                                dark
+                            >
+                                회원 가입
+                                <v-icon
+                                dark
+                                right
+                                >
+                                mdi-checkbox-marked-circle
+                                </v-icon>
+                            </v-btn>
+                        </v-row>
+                        </v-container>
                     </v-tab-item>
                 </v-tabs-items>
                
             </v-col>
             <v-col>
-                <img src="" alt="">
+                <v-img
+                lazy-src="https://picsum.photos/id/11/10/6"
+                max-height=""
+                max-width="500"
+                src="https://picsum.photos/id/11/500/300"
+                ></v-img>
             </v-col>
         </v-row>
-        <v-row>
-            <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            max-height="150"
-            max-width="250"
-            src="https://picsum.photos/id/11/500/300"
-            ></v-img>
-        </v-row>
-        <v-row>
-            <p>사용소개</p>
-        </v-row>
+        
     </v-container>
 </template>
 <script>
@@ -77,6 +122,9 @@ export default {
         return{
             tab:null,
             email:"",
+            login_email:"",
+            login_pwd:"",
+            items:['English', 'Korean'],
         }
     },
 }
