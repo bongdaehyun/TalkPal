b136f5c89b741f80663d552b4c10017a9221dc7a
봉대현 <pkjeogus@gmail.com>
2021-07-20 11:34:32 +0900

    Frontend- 불필요한 코드 삭제

--- front/src/main.js
+++ front/src/main.js
@@ -5,19 +5,7 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from '@/i18n'
 import InfiniteLoading from 'vue-infinite-loading';
-import Cookies from "js-cookie";
-import VueSession from 'vue-session'
 
-// const savedToken = Cookies.get("accessToken")
-// if(savedToken)
-// {
-//   store.dispatch('getDataByToken',savedToken)
-// }
-// var sessionOptions = {
-//   persist: true
-// }
-
-// Vue.use(VueSession, sessionOptions)
 
 Vue.config.productionTip = false;
 
