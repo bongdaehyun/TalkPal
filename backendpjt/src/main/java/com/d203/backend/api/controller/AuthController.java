a6735b3b6114a8f91749b44b1a8c9df4674864d8
봉대현 <pkjeogus@gmail.com>
2021-07-22 11:34:29 +0900

    Backend-로그인시 회원 인증 검사 구현완료

--- backendpjt/src/main/java/com/d203/backend/api/controller/AuthController.java
+++ backendpjt/src/main/java/com/d203/backend/api/controller/AuthController.java
@@ -34,6 +34,9 @@ public class AuthController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private AuthService authService;
+
 	@PostMapping("/login")
 	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
 	@ApiResponses({
@@ -50,17 +53,24 @@ public class AuthController {
 		if(user == null){
 			return ResponseEntity.ok(UserLoginPostRes.of(404, "NO USER", null));
 		}
+
 		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
 		if(passwordEncoder.matches(password, user.getPassword())) {
-			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(user)));
+			if (user.getValid()){
+				return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(user)));
+			}else{
+				return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "check yout email", null));
+			}
+		}else{
+			// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
+			return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 		}
-		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+
+
+
 	}
 
-	@Autowired
-	private AuthService authService;
+
 
 	@PostMapping
 	public ResponseEntity<?> sendEmail(String email){
