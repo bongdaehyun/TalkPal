0afdceeff8ec018f2886eea36a8edb16deede4e3
ragu6963 <bhj1684@naver.com>
2021-07-30 12:32:04 +0900

    Deploy - 오타수정

--- front/src/util/http-common.js
+++ front/src/util/http-common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  // baseURL: "/api/v1",
-  baseURL: process.env.VUE_APP_SERVER_URL,
+  baseURL: "/api/v1",
+  // baseURL: process.env.VUE_APP_SERVER_URL,
   headers: {
     "Content-type": "application/json"
   }
