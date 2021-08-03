cff44875aa8eaa67cbfc1b6e372eb5a4831c2e3a
bongdaehyun <pkjeogus@gmail.com>
2021-08-03 18:42:49 +0900

    Frontend- follow관련 무한스크롤

--- front/src/components/Profile/FollowingModal.vue
+++ front/src/components/Profile/FollowingModal.vue
@@ -4,9 +4,10 @@
         <v-card-title>Select Country</v-card-title>
         <v-divider></v-divider>
         <v-card-text style="height: 300px;">
-         
-            <Content/>
-    
+         <div v-for="follow in follows" :key="follow.id" lg="4" md="3" sm="2" xs="1">
+            <Content :item="follow"/>
+          </div>
+          <infinite-loading @infinite="getFollow"></infinite-loading>
         </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
@@ -30,17 +31,25 @@
 </template>
 
 <script>
-
+import InfiniteLoading from "vue-infinite-loading";
 import Content from "@/components/Profile/Follow/followitems.vue";
+
   export default {
+    props: {
+      follow: {
+        type: String,
+      },
+    },
     data () {
       return {
-        dialogm1: '',
         dialog: false,
-        notifications:[],
+        follows:[],
+        userid : this.$route.params.userId,
+        pageno : 1,
       }
     },
     created() {
+      console.log("실행",this.follow)
       console.log( this.$route.params.userId)
       let url = "userStore/listFollower"
       let followInfo ={
@@ -49,13 +58,33 @@ import Content from "@/components/Profile/Follow/followitems.vue";
         }
         this.$store.dispatch(url,followInfo)
         .then((res) => {
-          console.log(res.data.followList)
-          this.notifications = res.data.followList;
+          this.follows = res.data.followList;
         })
     },
+    methods:{
+      getFollow($state){
+           let url = "userStore/listFollower"
+          let followInfo ={
+            userid : this.userid,
+            pageno : this.pageno
+          }
+        this.$store.dispatch(url,followInfo)
+        .then((res) => {
+          console.log("ㅇㅇㅇ",res.data.followList)
+          const data = res.data.followList;
+          if (data.length) {
+            this.follows.push(...data);
+            this.pageno += 1;
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        })
+      }
+    },
     components: {
-    Content
-    
+      Content,
+      InfiniteLoading,
   },
   }
 </script>
