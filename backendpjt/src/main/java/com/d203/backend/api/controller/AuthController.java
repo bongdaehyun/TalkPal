2632d6c8a6ef7fbcbda644c31afb8bf0df03c626
봉대현 <pkjeogus@gmail.com>
2021-08-03 00:17:57 +0000

    Update AuthController.java

--- backendpjt/src/main/java/com/d203/backend/api/controller/AuthController.java
+++ backendpjt/src/main/java/com/d203/backend/api/controller/AuthController.java
@@ -75,7 +75,7 @@ public class AuthController {
 		try{
 			User user=userService.getUserByEamil(email);
 			authService.sendVerificationMail(user);
-			System.out.println("sendmail --- Success");
+			System.out.println("sendmail d--- Success");
 			return new ResponseEntity<String>("성공",HttpStatus.OK);
 		}catch (Exception e){
 			return new ResponseEntity<String>("오류", HttpStatus.BAD_REQUEST);
